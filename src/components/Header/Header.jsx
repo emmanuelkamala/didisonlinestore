a18b6214e8d9345fb8e48b54e7f6ee8154307326
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-11-07 01:47:51 +0300

    Create header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+const Header = () => {
+  return (
+    <div>
+      <Typography variant="h6" color="textSecondary">
+        Header
+      </Typography>
+    </div>
+  )
+}
+
+export default Header;
