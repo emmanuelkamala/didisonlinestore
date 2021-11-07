b555aeeb728a9f71877ecd90ea8fceb32ee3f0de
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-11-07 18:30:56 +0300

    Add product component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Container } from 'react-bootstrap';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
 
@@ -8,7 +9,9 @@ function App() {
     <Container fluid>
       <Header />
       <main className="py-3">
-        Hi There
+        <Container>
+          <HomeScreen />
+        </Container>
       </main>
       <Footer />
     </Container>
---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import Product from '../components/product/Product';
+import products from '../products';
+
+const Homescreen = () => {
+  return (
+    <div>
+      <h3>Best Selling Products</h3>
+      <Row>
+        {
+          products.map((product) => (
+            <Col sm={12} md={6} lg={4} xl={3}>
+              <Product product={product}/>
+            </Col>
+          ))
+        }
+      </Row>
+    </div>
+  )
+}
+
+export default Homescreen;
