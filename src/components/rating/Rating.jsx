f9b938d2c7aef963b283614e32036acadd3cb24b
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-11-08 10:28:59 +0300

    Edit Rating component

--- src/components/rating/Rating.jsx
+++ src/components/rating/Rating.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import { StarBorderOutlined, StarHalfOutlined, StarOutlined } from '@mui/icons-material';
 
-const Rating = ({ value, text }) => {
+const Rating = ({ value, text, color }) => {
   return (
     <div className="rating">
-      <span>
+      <span style={{ color }}>
         { value >= 1 ? <StarOutlined /> : value >= 0.5 ? <StarHalfOutlined /> : <StarBorderOutlined /> }
       </span>
 
-      <span>
+      <span style={{ color }}>
         { value >= 2 ? <StarOutlined /> : value >= 1.5 ? <StarHalfOutlined /> : <StarBorderOutlined /> }
       </span>
-      <span>
+      <span style={{ color }}>
         { value >= 3 ? <StarOutlined /> : value >= 2.5 ? <StarHalfOutlined /> : <StarBorderOutlined /> }
       </span>
-      <span>
+      <span style={{ color }}>
         { value >= 4 ? <StarOutlined /> : value >= 3.5 ? <StarHalfOutlined /> : <StarBorderOutlined /> }
       </span>
-      <span>
+      <span style={{ color }}>
         { value >= 5 ? <StarOutlined /> : value >= 4.5 ? <StarHalfOutlined /> : <StarBorderOutlined /> }
       </span>
       <span>{ text && text }</span>
     </div>
   )
+}
+
+Rating.defaultProps = {
+  color: "#FFD700"
 }
 
 export default Rating;
