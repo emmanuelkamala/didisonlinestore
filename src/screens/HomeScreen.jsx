e0c267d0a18b92da76b7a575d420c74cbfbb6428
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-11-10 00:54:16 +0300

    Fetch products from server

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import Product from '../components/product/Product';
-import products from '../../backend/data/products';
 
 const Homescreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data)
+    }
+
+    fetchProducts();
+  }, []);
+
   return (
     <div>
       <h3>Best Selling Products</h3>
@@ -11,7 +22,7 @@ const Homescreen = () => {
         {
           products.map((product) => (
             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product}/>
+              <Product product={product} />
             </Col>
           ))
         }
--- src/screens/ProductScreen.jsx
+++ src/screens/ProductScreen.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/rating/Rating';
-import list from '../../backend/data/products';
+import list from '../products';
 
 const ProductScreen = () => {
   const { id } = useParams();
