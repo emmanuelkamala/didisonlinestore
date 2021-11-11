1a53419101652beca56dd0577abc391d4ede3f54
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-11-12 00:00:15 +0300

    Create cart actions

--- src/store.js
+++ src/store.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers.jsx';
+import { cartReducer } from './reducers/cartReducers.jsx';
 
 const reducer = combineReducers({
   productList: productListReducer,
-  productDetails: productDetailsReducer
+  productDetails: productDetailsReducer,
+  cart: cartReducer
 });
 
 const initialState = {};
