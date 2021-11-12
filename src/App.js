4963ee4fd2aa8ab4f6168ec28eec7ae94dcc175c
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-11-12 09:19:21 +0300

    Create a cart screen functionality

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <main className="py-3">
           <Container>
             <Routes>
-              <Route path='/' element = { <HomeScreen/> } exact />
+              <Route path='/' element = { <HomeScreen /> } exact />
               <Route path='/product/:id' element = { <ProductScreen /> } />
               <Route path='/cart/:id?' element = { <CartScreen /> } />
             </Routes>
