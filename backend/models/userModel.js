2984af0a47e88ff894dd8f474e93065736867419
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-11-12 14:39:56 +0300

    Create a user controller

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = mongoose.Schema({
   name: { type: String, required: true },
@@ -6,8 +7,14 @@ const userSchema = mongoose.Schema({
   password: { type: String, required: true },
   isAdmin: { type: Boolean, required: true, default: false }
 }, 
-  timestamps
+  {
+    timestamps: true
+  }
 )
+
+userSchema.methods.matchPassword = async function (enteredPassword){
+  return await bcrypt.compare(enteredPassword, this.password);
+}
 
 const User = mongoose.model('User', userSchema);
 
