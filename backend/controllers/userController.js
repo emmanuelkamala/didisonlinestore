e60ed18d69d2095eccf9e579b6b887fc8f4be3ba
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-11-12 15:06:49 +0300

    Generate token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -13,7 +14,7 @@ const authUser = asyncHandler(async (req, res) => {
       email: user.email,
       password: user.password,
       isAdmin: user.isAdmin,
-      token: null
+      token: generateToken(user._id)
     })
   } else {
     res.status(401);
