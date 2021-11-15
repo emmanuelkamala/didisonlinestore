425af5aaf0f354f34781b46eae5bde9ef5da9165
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-11-15 14:02:50 +0300

    Create a login authorized user in backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,7 +23,20 @@ const authUser = asyncHandler(async (req, res) => {
 })
 
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send('profile')
+  const user = await User.findById(req.user._id);
+
+  if (user){
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
  
 })
 
