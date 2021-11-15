d4774cde53b793b66fd0e31f3ffa00ab61e17a7d
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-11-15 16:47:41 +0300

    Edit functionality in login screen

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { Row, Col, Button, Form } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import Message from '../components/Message';
@@ -9,26 +9,41 @@ import FormContainer from '../components/FormContainer';
 
 const LoginScreen = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
 
-  const redirect = location.search ? Number(location.search.split('=')[1]) : '/';
+  const userLogin = useSelector(state => state.userLogin);
+
+  const { loading, error, userInfo } = userLogin;
+
+  const redirect = location.search ? location.search.split('=')[1] : '/';
+
+  useEffect(() => {
+    if (userInfo){
+      navigate('/redirect')
+    }
+  }, [navigate, redirect, userInfo])
 
   const submitHandler = e => {
     e.preventDefault();
+    dispatch(login(email, password))
   }
 
   return (
     <FormContainer>
       <h1>Sign In</h1>
+      { error && <Message variant='danger'>{error}</Message> }
+      { loading && <Loader /> }
       <Form onSubmit={submitHandler}>
         <Form.Group controlId='email' className='my-2'>
           <Form.Label>Email Address</Form.Label>
-          <Form.Control type='email' placeholder='Enter email' value={email} onChange={(e)=> e.target.value}></Form.Control>
+          <Form.Control type='email' placeholder='Enter email' value={email} onChange={(e)=> setEmail(e.target.value)}></Form.Control>
         </Form.Group>
         <Form.Group controlId='password' className='my-2'>
           <Form.Label>Password</Form.Label>
-          <Form.Control type='password' placeholder='Enter password' value={password} onChange={(e)=> e.target.value}></Form.Control>
+          <Form.Control type='password' placeholder='Enter password' value={password} onChange={(e)=> setPassword(e.target.value)}></Form.Control>
         </Form.Group>
 
         <Button type='submit' variant='primary' className='my-2'>Sign In</Button>
