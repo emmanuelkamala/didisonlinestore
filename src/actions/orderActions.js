106fc72bba138a88f0a63640b726a7fb74eef3ca
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-11-16 18:58:59 +0300

    Edit place order screen functionality

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS } from "../constants/orderConstants";
 
 export const createOrder = (order) => async (dispatch, getState) => {
@@ -15,7 +16,7 @@ export const createOrder = (order) => async (dispatch, getState) => {
       }
     }
 
-    const { data } = await axios.post('/api/orders', order, config);
+    const { data } = await axios.post(`/api/orders`, order, config);
 
     dispatch({
       type: ORDER_CREATE_SUCCESS,
--- src/screens/PlaceOrderScreen.jsx
+++ src/screens/PlaceOrderScreen.jsx
@@ -1,23 +1,44 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button, Col, Row, ListGroup, Image, Card } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import Message from '../components/Message';
 import CheckoutSteps from '../components/CheckoutSteps';
 import { Link } from 'react-router-dom';
+import { createOrder } from '../actions/orderActions';
 
 const PlaceOrderScreen = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const cart = useSelector(state => state.cart); 
 
   // Calculate prices
   cart.itemsPrice = cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0);
   cart.shippingPrice = cart.itemsPrice < 100 ? 0 : 100;
   cart.taxPrice = Number((cart.itemsPrice * 0.18).toFixed(2));
-  cart.totalPrice = cart.itemsPrice + cart.shippingPrice + cart.taxPrice;
+  cart.totalPrice = (cart.itemsPrice + cart.shippingPrice + cart.taxPrice).toFixed(2);
 
+  const orderCreate = useSelector(state => state.orderCreate);
+  const { order, success, error } = orderCreate;
+
+  useEffect(() => {
+    if(success){
+      navigate(`/order/${order._id}`)
+    }
+    // eslint-disable-next-line
+  }, [navigate, success])
 
   const placeOrderHandler = (e) => {
-    e.preventDefault()
+    e.preventDefault();
+    dispatch(createOrder({
+      orderItems: cart.cartItems,
+      shippingAddress: cart.shippingAddress,
+      paymentMethod: cart.paymentMethod,
+      itemsPrice: cart.itemsPrice,
+      shippingPrice: cart.shippingPrice,
+      taxPrice: cart.taxPrice,
+      totalPrice: cart.totalPrice,
+    }))
   }
 
   return (
@@ -53,7 +74,7 @@ const PlaceOrderScreen = () => {
                         <ListGroup.Item key={index}>
                           <Row>
                             <Col md={1}>
-                              <Image src={item.image} src={item.name} fluid rounded />
+                              <Image src={item.image} alt={item.name} fluid rounded />
                             </Col>
                             <Col>
                               <Link to={`/product/${item.product}`}>
@@ -106,6 +127,12 @@ const PlaceOrderScreen = () => {
                   <Col>Total</Col>
                   <Col>${cart.totalPrice}</Col>
                 </Row>  
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                {
+                  error && <Message variant='danger'>{error}</Message>
+                }
               </ListGroup.Item>
 
               <ListGroup.Item>
